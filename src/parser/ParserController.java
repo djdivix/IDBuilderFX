10839241f376df5705c37528a62825518b84a92a
djdivix <divyanshu.divix@gmail.com>
2017-02-15 14:33:00 +0530

    fix multifile parsing console print

--- src/parser/ParserController.java
+++ src/parser/ParserController.java
@@ -21,6 +21,8 @@ public class ParserController {
 		
 		ArrayList<String> multiFiles = MultiFileChooserController.getSelectedFiles();
 		
+		String name1="",email1="",phone1="",address1="",dob1="",gender1="",fatherName1="";
+		
 		if(multiFiles.isEmpty())
 		{
 		String singlefilePath = singleFile.getAbsolutePath().replaceAll("/", "//");
@@ -28,43 +30,33 @@ public class ParserController {
 		System.out.println(text);
 		System.out.println(
 					"______________________________________________________________________________________________________________");
-		}
-		else{
-			for (String i : multiFiles) {
-				text = ExtractText.extractPdf(i.replaceAll("/", "//"));
-				System.out.println(text);
-				System.out.println(
-						"______________________________________________________________________________________________________________");
-			}
-		}
-
-		String name1 = FetchName.findName(text).trim();
+		name1 = FetchName.findName(text).trim();
 		System.out.println("NAME   " + name1);
 		text = text.replaceAll(name1, "");
 		// Email
-		String email1 = FetchEmail.findEmail(text).trim();
+		email1 = FetchEmail.findEmail(text).trim();
 		System.out.println("EMAIL   " + email1);
 		text = text.replaceAll(email1, "");
 		// Phone
-		String phone1 = FetchPhone.findPhone(text).trim();
+		phone1 = FetchPhone.findPhone(text).trim();
 		System.out.println("PHONE   " + phone1);
 		if (phone1.charAt(0) == '+')
 			phone1 = phone1.substring(1);
 		text = text.replaceAll(phone1, "");
 		// Address
-		String address1 = FetchAddress.findAddress(text).trim();
+		address1 = FetchAddress.findAddress(text).trim();
 		System.out.println("ADDRESS  " + address1);
 		text = text.replaceAll(address1, "");
 		// Date Of Birth
-		String dob1 = FetchDob.findDob(text).trim();
+		dob1 = FetchDob.findDob(text).trim();
 		System.out.println("DATE OF BIRTH  " + dob1);
 		text = text.replaceAll(dob1, "");
 		// Gender
-		String gender1 = FetchGender.findGender(text).trim();
+		gender1 = FetchGender.findGender(text).trim();
 		System.out.println("GENDER  " + gender1);
 		text = text.replaceAll(gender1, "");
 		// Father's Name
-		String fatherName1 = FetchFatherName.findFatherName(text).trim();
+		fatherName1 = FetchFatherName.findFatherName(text).trim();
 		System.out.println("FATHER'S NAME  " + fatherName1);
 		text = text.replaceAll(fatherName1, "");
 		System.out.println(
@@ -73,11 +65,60 @@ public class ParserController {
 		// System.out.println("______________________________________________________________________________________________________________");
 		System.out.println(
 				"______________________________________________________________________________________________________________");
+		empid += 1;
+		database.StoreEmployeeDetails.storeDetails(empid, name1, email1, phone1, address1, dob1, gender1, fatherName1);
+		}
+		else{
+			for (String i : multiFiles) {
+				text = ExtractText.extractPdf(i.replaceAll("/", "//"));
+				System.out.println(text);
+				System.out.println(
+						"______________________________________________________________________________________________________________");
+				name1 = FetchName.findName(text).trim();
+				System.out.println("NAME   " + name1);
+				text = text.replaceAll(name1, "");
+				// Email
+				email1 = FetchEmail.findEmail(text).trim();
+				System.out.println("EMAIL   " + email1);
+				text = text.replaceAll(email1, "");
+				// Phone
+				phone1 = FetchPhone.findPhone(text).trim();
+				System.out.println("PHONE   " + phone1);
+				if (phone1.charAt(0) == '+')
+					phone1 = phone1.substring(1);
+				text = text.replaceAll(phone1, "");
+				// Address
+				address1 = FetchAddress.findAddress(text).trim();
+				System.out.println("ADDRESS  " + address1);
+				text = text.replaceAll(address1, "");
+				// Date Of Birth
+				dob1 = FetchDob.findDob(text).trim();
+				System.out.println("DATE OF BIRTH  " + dob1);
+				text = text.replaceAll(dob1, "");
+				// Gender
+				gender1 = FetchGender.findGender(text).trim();
+				System.out.println("GENDER  " + gender1);
+				text = text.replaceAll(gender1, "");
+				// Father's Name
+				fatherName1 = FetchFatherName.findFatherName(text).trim();
+				System.out.println("FATHER'S NAME  " + fatherName1);
+				text = text.replaceAll(fatherName1, "");
+				System.out.println(
+						"______________________________________________________________________________________________________________");
+				System.out.println(text);
+				// System.out.println("______________________________________________________________________________________________________________");
+				System.out.println(
+						"______________________________________________________________________________________________________________");
+				empid += 1;
+				database.StoreEmployeeDetails.storeDetails(empid, name1, email1, phone1, address1, dob1, gender1, fatherName1);
+			}
+		}
+
+
 		// name.setText(FetchName.findName(text));
 		// email.setText(FetchEmail.findEmail(text));
 		// Now Storing in database
-		empid += 1;
-		database.StoreEmployeeDetails.storeDetails(empid, name1, email1, phone1, address1, dob1, gender1, fatherName1);
+
 	}
 
 }
