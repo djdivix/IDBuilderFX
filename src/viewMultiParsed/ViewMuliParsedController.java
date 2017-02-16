83ba3c690835a0d8e003d56f1961f68f565a996c
djdivix <divyanshu.divix@gmail.com>
2017-02-17 00:48:28 +0530

    Styling of tableview
    Make size of tableview in multi parsed view adjustable.
    Add text wrapping and css styling.

--- src/viewMultiParsed/ViewMuliParsedController.java
+++ src/viewMultiParsed/ViewMuliParsedController.java
@@ -6,66 +6,115 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Control;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 public class ViewMuliParsedController implements Initializable {
 	@FXML
 	private TableView<EmployeeClass> table;
 	@FXML
-	private TableColumn<EmployeeClass,String> name;
+	private TableColumn<EmployeeClass, String> name;
 	@FXML
-	private TableColumn<EmployeeClass,String> email;
+	private TableColumn<EmployeeClass, String> email;
 	@FXML
-	private TableColumn<EmployeeClass,String> mobile;
+	private TableColumn<EmployeeClass, String> mobile;
 	@FXML
-	private TableColumn<EmployeeClass,String> add;
+	private TableColumn<EmployeeClass, String> add;
 	@FXML
-	private TableColumn<EmployeeClass,String> dob;
+	private TableColumn<EmployeeClass, String> dob;
 	@FXML
-	private TableColumn<EmployeeClass,String> sex;
+	private TableColumn<EmployeeClass, String> sex;
 	@FXML
-	private TableColumn<EmployeeClass,String> fname;
-	
+	private TableColumn<EmployeeClass, String> fname;
+
 	private ObservableList<EmployeeClass> data;
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		name.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("name"));
-		email.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("email"));
-		mobile.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("mobile"));
-		add.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("add"));
-		dob.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("dob"));
-		sex.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("sex"));
-		fname.setCellValueFactory(new PropertyValueFactory<EmployeeClass,String>("fname"));
-		
-	    try {
+
+		name.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("name"));
+		email.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("email"));
+		mobile.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("mobile"));
+		add.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("add"));
+		dob.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("dob"));
+		sex.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("sex"));
+		fname.setCellValueFactory(new PropertyValueFactory<EmployeeClass, String>("fname"));
+
+		try {
 			buildData();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	 
-	    
-	    
+
 	}
-	
-	public void buildData() throws SQLException{
+
+	public void buildData() throws SQLException {
 		data = FXCollections.observableArrayList();
-		Connection c ;	
+		Connection c;
 		c = database.SqliteConnection.getConnection();
 		String SQL = "SELECT * from Employee";
-	    ResultSet rs = c.createStatement().executeQuery(SQL);
-	    
-	    while(rs.next()){
-	    	EmployeeClass ec = new EmployeeClass(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6),rs.getString(7), rs.getString(8));
-	    	data.add(ec);
+		ResultSet rs = c.createStatement().executeQuery(SQL);
+
+		while (rs.next()) {
+			EmployeeClass ec = new EmployeeClass(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+					rs.getString(6), rs.getString(7), rs.getString(8));
+			data.add(ec);
+		}
+		
+		//Add text wrapping in columns
+		add.setCellFactory(new Callback<TableColumn<EmployeeClass, String>, TableCell<EmployeeClass, String>>() {
+
+	        @Override
+	        public TableCell<EmployeeClass, String> call(
+	                TableColumn<EmployeeClass, String> param) {
+	            TableCell<EmployeeClass, String> cell = new TableCell<>();
+	            Text text = new Text();
+	            cell.setGraphic(text);
+	            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+	            text.wrappingWidthProperty().bind(add.widthProperty());
+	            text.textProperty().bind(cell.itemProperty());
+	            return cell ;
+	        }
+	    });
+		name.setCellFactory(new Callback<TableColumn<EmployeeClass, String>, TableCell<EmployeeClass, String>>() {
+
+	        @Override
+	        public TableCell<EmployeeClass, String> call(
+	                TableColumn<EmployeeClass, String> param) {
+	            TableCell<EmployeeClass, String> cell = new TableCell<>();
+	            Text text = new Text();
+	            cell.setGraphic(text);
+	            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+	            text.wrappingWidthProperty().bind(name.widthProperty());
+	            text.textProperty().bind(cell.itemProperty());
+	            return cell ;
+	        }
+	    });
+		fname.setCellFactory(new Callback<TableColumn<EmployeeClass, String>, TableCell<EmployeeClass, String>>() {
+
+	        @Override
+	        public TableCell<EmployeeClass, String> call(
+	                TableColumn<EmployeeClass, String> param) {
+	            TableCell<EmployeeClass, String> cell = new TableCell<>();
+	            Text text = new Text();
+	            cell.setGraphic(text);
+	            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+	            text.wrappingWidthProperty().bind(fname.widthProperty());
+	            text.textProperty().bind(cell.itemProperty());
+	            return cell ;
+	        }
+	    });
+		table.setItems(data);
 	}
-	    table.setItems(data);
-}}
+}
