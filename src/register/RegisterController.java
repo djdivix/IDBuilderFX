04ee4a9abc57ecff2bc0c89f2e2065cf9ae920d5
djdivix <divyanshu.divix@gmail.com>
2017-04-01 16:29:53 +0530

    Register CSS

--- src/register/RegisterController.java
+++ src/register/RegisterController.java
@@ -32,94 +32,124 @@ public class RegisterController {
 
 	public void confRegister(ActionEvent event) throws IOException, SQLException {
 		
-		Connection c;
+		nametxt.getStyleClass().remove("error");
+		usertxt.getStyleClass().remove("error");
+		emailtxt.getStyleClass().remove("error");
+		passtxt1.getStyleClass().remove("error");
+		passtxt2.getStyleClass().remove("error");
+		
+		Connection c=null;
 		c = database.SqliteConnection.getConnection();
 		String SQL = "SELECT * from Users";
 		ResultSet rs = c.createStatement().executeQuery(SQL);
-	
+		//Name validation
 		if (nametxt.getText().equals("")) {
 			errlbl.setText("Name cannot be empty!");
+			nametxt.getStyleClass().add("error");
 			return;
 		}
 		Pattern r = Pattern.compile("[0-9]");
 		Matcher m = r.matcher(nametxt.getText());
 		if (m.find()) {
 			errlbl.setText("Name cannot contain number!");
+			nametxt.getStyleClass().add("error");
 			return;
 		}
-		if (usertxt.getText().equals("")) {
-			errlbl.setText("Username cannot be empty!");
-			return;
-		}
-		r = Pattern.compile("^\\w+$");
-		m = r.matcher(usertxt.getText());
-	    if(!m.find()) {
-	    errlbl.setText("Username must contain only letters, numbers and underscores!");
-	      return;
-	    }
-	    while(rs.next())
-	    {
-	    	if(usertxt.getText().equals(rs.getString(1)))
-	    	{
-	    		errlbl.setText("Username is already taken. Please try another one.");
-	    		return;
-	    	}
-	    	if(emailtxt.getText().equals(rs.getString(4)))
-	    	{
-	    		errlbl.setText("This E-mail is already registered. Enter another E-mail.");
-	    		return;
-	    	}
-	    }
+		//Email validation
 		r = Pattern.compile(
 				"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 		m = r.matcher(emailtxt.getText());
 		if (emailtxt.getText().equals("")) {
 			errlbl.setText("Email cannot be empty!");
+			emailtxt.getStyleClass().add("error");
 			return;
 		}
 		if (!m.find()) {
 			errlbl.setText("Enter Valid EmaiL!");
+			emailtxt.getStyleClass().add("error");
 			return;
 		}
+		//Username validation
+		if (usertxt.getText().equals("")) {
+			errlbl.setText("Username cannot be empty!");
+			usertxt.getStyleClass().add("error");
+			return;
+		}
+		r = Pattern.compile("^\\w+$");
+		m = r.matcher(usertxt.getText());
+	    if(!m.find()) {
+	    	errlbl.setText("Username must contain only letters, numbers and underscores!");
+	    	usertxt.getStyleClass().add("error");
+	      return;
+	    }
+	    //Check Unique username and email
+	    while(rs.next())
+	    {
+	    	if(emailtxt.getText().equals(rs.getString(4)))
+	    	{
+	    		errlbl.setText("This E-mail is already registered. Enter another E-mail.");
+	    		emailtxt.getStyleClass().add("error");
+	    		c.close();
+	    		return;
+	    	}
+	    	if(usertxt.getText().equals(rs.getString(1)))
+	    	{
+	    		errlbl.setText("Username is already taken. Please try another one.");
+	    		usertxt.getStyleClass().add("error");
+	    		c.close();
+	    		return;
+	    	}
+	    }
+		//Password Validation
 		if (passtxt1.getText().equals("")) {
 			errlbl.setText("Password cannot be empty!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		if (passtxt1.getText().length() < 6) {
 			errlbl.setText("Password must contain at least six characters!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		if (passtxt1.getText().equals(usertxt.getText())) {
 			errlbl.setText("Password must be different from Username!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		r = Pattern.compile("[0-9]");
 		m = r.matcher(passtxt1.getText());
 		if (!m.find()) {
 			errlbl.setText("Password must contain at least one number (0-9)!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		r = Pattern.compile("[a-z]");
 		m = r.matcher(passtxt1.getText());
 		if (!m.find()) {
 			errlbl.setText("Password must contain at least one lowercase letter (a-z)!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		r = Pattern.compile("[A-Z]");
 		m = r.matcher(passtxt1.getText());
 		if (!m.find()) {
 			errlbl.setText("Password must contain at least one uppercase letter (A-Z)!");
+			passtxt1.getStyleClass().add("error");
 			return;
 		}
 		if (passtxt2.getText().equals("")) {
 			errlbl.setText("Please retype your password to confirm!");
+			passtxt2.getStyleClass().add("error");
 			return;
 		}
 		if (!passtxt1.getText().equals(passtxt2.getText())) {
 			errlbl.setText("Passwords do not match!");
+			passtxt1.getStyleClass().add("error");
+			passtxt2.getStyleClass().add("error");
 			return;
 		}
 		database.StoreUserDetails.storeDetails(usertxt.getText(), passtxt1.getText() , nametxt.getText(), emailtxt.getText());
+		c.close();
 		REGISTERED=1;
 		Stage currstage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		currstage.close();
